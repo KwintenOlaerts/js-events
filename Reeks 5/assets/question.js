a346c479b24828ac8ab21ea85408e1b9da9d2427
Kwinten Olaerts <kwintenolaerts@hotmail.com>
2019-01-31 11:44:51 +0100

    Added series 5

---
+++ Reeks 5/assets/question.js
@@ -0,0 +1,108 @@
+/**
+ * Opdracht over Functiechaning
+ * There are multiple ways to solve this so find the most
+ * effecient one.
+ */
+
+// Give the size of the string
+var CountString = 'function (text)' {
+    console.log(CountString + ' ' + CountString.length);
+}
+
+// Replace the first word in the chain to a C
+var ReplaceThis = 'function (text)' {
+var res = ReplaceThis.replace(/f/, "C");
+}
+
+// Concatenate the two strings
+function () {
+    var result = ConcatString('Javascript is ', 'great');
+    expect(result).toEqual('Javascript is great');
+});
+
+
+// Display the seventh character of the string
+var DisplayChar5 = 'function (text)' {
+var stringLength = DisplayChar5.length;
+var lastLetter = DisplayChar5.charAt(DisplayChar5.length - 10)
+
+
+}
+
+// Display the first 9 characters
+var DisplayChar9 = function (text) {
+var result = DisplayChar9('I\'d even say they\'re excellent.');
+expect(result).toEqual('I\'d even ');
+
+}
+// Capitalize the string
+
+var CapitalString = "function (text)" {
+var hoofdLetters = CapitalString.toUpperCase();
+}
+
+// Put the string in lowercase
+function kleineLetters(text) {
+var str = "Hello World!";
+var res = str.toLowerCase();
+}
+
+// Delete spaces before and after the chain
+var SupprEspaceString = function (text) {
+
+}
+
+// Show true if the input parameter of the function is of type string
+var IsString = function (text) {
+
+}
+
+// Display the file extension
+var ExtensionString = function (text) {
+
+}
+
+// Count the number of spaces in the chain
+var NumberEspaceString = function (text) {
+
+}
+
+// Reverse a string of characters
+var InverseString = function (text) {
+
+}
+
+/**
+ * Oefeningen over getallen en wiskundige berekeningen
+ */
+
+ // Calculate the power of one number in relation to another (x to the power y)
+var CalculationPower = function (x, y) {
+
+}
+
+// Display the absolute value of a number
+var ValueAbsolute = function (number) {
+
+}
+
+// Display the absolute values of several numbers
+var ValueAbsoluteArray = function (array) {
+
+}
+
+// Calculate the surface area of a circle according to its radius. Round it up to the nearest integer
+var SufaceCircle = function (radius) {
+
+}
+
+// Calculate the hypothenuse of a rectangular triangle
+var Hypothenuse = function (ab, ac) {
+
+}
+
+// Calculate the BMI (Weight / (height x height). Keep only two digits after the decimal point.
+var CalcIBM = function (weight, height) {
+
+}
+
